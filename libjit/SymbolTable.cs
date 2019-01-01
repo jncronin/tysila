c76ac8ffd8e74b16c7c80cf576b44f8948786621
John Cronin <jncronin@tysos.org>
2019-01-01 20:36:58 +0000

    Fix-up SymbolTable for libjit

--- libjit/SymbolTable.cs
+++ libjit/SymbolTable.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright (C) 2008 - 2011 by John Cronin
+﻿/* Copyright (C) 2008 - 2019 by John Cronin
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,14 +29,14 @@ namespace tysos
     {
         internal abstract class SymbolProvider
         {
-            internal protected abstract ulong GetAddress(string s);
-            internal protected abstract string GetSymbol(ulong address);
-            internal protected abstract string GetSymbolAndOffset(ulong address, out ulong offset);
+            internal protected abstract void* GetAddress(string s);
+            internal protected abstract string GetSymbol(void* address);
+            internal protected abstract string GetSymbolAndOffset(void* address, out UIntPtr offset);
         }
 
-        internal Dictionary<string, ulong> sym_to_offset;
-        internal metadata.Collections.SortedList<ulong, string> offset_to_sym;
-        internal Dictionary<string, ulong> sym_to_length;
+        internal Dictionary<string, UIntPtr> sym_to_offset;
+        internal metadata.Collections.SortedList<UIntPtr, string> offset_to_sym;
+        internal Dictionary<string, UIntPtr> sym_to_length;
 
         internal List<ulong> static_fields_addresses = new List<ulong>();
         internal List<ulong> static_fields_lengths = new List<ulong>();
@@ -44,65 +44,43 @@ namespace tysos
 
         public SymbolTable()
         {
-            sym_to_offset = new Dictionary<string, ulong>(0x20000, new Program.MyGenericEqualityComparer<string>());
-            offset_to_sym = new metadata.Collections.SortedList<ulong, string>(0x20000, new Program.MyComparer<ulong>());
-            sym_to_length = new Dictionary<string, ulong>(0x20000, new Program.MyGenericEqualityComparer<string>());
-
-            unsafe
-            {
-                ulong ots = libsupcs.CastOperations.ReinterpretAsUlong(offset_to_sym);
-                Formatter.Write("offset_to_sym: ", Program.arch.DebugOutput);
-                Formatter.Write(ots, "X", Program.arch.DebugOutput);
-                Formatter.Write(", vtable: ", Program.arch.DebugOutput);
-                Formatter.Write(*(ulong*)ots, "X", Program.arch.DebugOutput);
-                Formatter.Write(", ti: ", Program.arch.DebugOutput);
-                Formatter.Write(**(ulong**)ots, "X", Program.arch.DebugOutput);
-                Formatter.WriteLine(Program.arch.DebugOutput);
-
-            }
-
+            sym_to_offset = new Dictionary<string, UIntPtr>(0x20000, new libsupcs.GenericEqualityComparer<string>());
+            offset_to_sym = new metadata.Collections.SortedList<UIntPtr, string>(0x20000, new libsupcs.UIntPtrComparer());
+            sym_to_length = new Dictionary<string, UIntPtr>(0x20000, new libsupcs.GenericEqualityComparer<string>());
         }
 
-        public void Add(string sym, ulong address)
+        public void Add(string sym, void* address)
         {
             unsafe
             {
                 ulong ots = libsupcs.CastOperations.ReinterpretAsUlong(offset_to_sym);
-                Formatter.Write("offset_to_sym: ", Program.arch.DebugOutput);
-                Formatter.Write(ots, "X", Program.arch.DebugOutput);
-                Formatter.Write(", vtable: ", Program.arch.DebugOutput);
-                Formatter.Write(*(ulong*)ots, "X", Program.arch.DebugOutput);
-                Formatter.Write(", ti: ", Program.arch.DebugOutput);
-                Formatter.Write(**(ulong**)ots, "X", Program.arch.DebugOutput);
-                Formatter.WriteLine(Program.arch.DebugOutput);
-
             }
             if (sym_to_offset.ContainsKey(sym))
             {
-                Formatter.Write("Warning: duplicate symbol: ", Program.arch.DebugOutput);
-                Formatter.WriteLine(sym, Program.arch.DebugOutput);
+                System.Diagnostics.Debugger.Log(0, "libjit", "SymbolTable.Add: Warning: duplicate symbol: " +
+                    sym);
             }
             else
-                sym_to_offset.Add(sym, address);
-            if(!offset_to_sym.ContainsKey(address))
-                offset_to_sym.Add(address, sym);
+                sym_to_offset.Add(sym, (UIntPtr)address);
+            if(!offset_to_sym.ContainsKey((UIntPtr)address))
+                offset_to_sym.Add((UIntPtr)address, sym);
         }
 
-        public void Add(string sym, ulong address, ulong length)
+        public void Add(string sym, void* address, UIntPtr length)
         {
             if (sym_to_offset.ContainsKey(sym))
             {
-                Formatter.Write("Warning: duplicate symbol: ", Program.arch.DebugOutput);
-                Formatter.WriteLine(sym, Program.arch.DebugOutput);
+                System.Diagnostics.Debugger.Log(0, "libjit", "SymbolTable.Add: Warning: duplicate symbol: " +
+                    sym);
             }
             else
             {
-                sym_to_offset.Add(sym, address);
+                sym_to_offset.Add(sym, (UIntPtr)address);
                 sym_to_length.Add(sym, length);
             }
             
-            if (!offset_to_sym.ContainsKey(address))
-                offset_to_sym.Add(address, sym);
+            if (!offset_to_sym.ContainsKey((UIntPtr)address))
+                offset_to_sym.Add((UIntPtr)address, sym);
         }
 
         public void AddStaticField(ulong address, ulong length)
@@ -111,22 +89,22 @@ namespace tysos
             static_fields_lengths.Add(length);
         }
 
-        public ulong GetAddress(string sym)
+        public void* GetAddress(string sym)
         {
             foreach (SymbolProvider sp in symbol_providers)
             {
-                ulong ret = sp.GetAddress(sym);
-                if (ret != 0)
+                var ret = sp.GetAddress(sym);
+                if (ret != null)
                     return ret;
             }
 
             if (sym_to_offset.ContainsKey(sym))
-                return sym_to_offset[sym];
+                return (void*)sym_to_offset[sym];
             else
-                return 0;
+                return (void*)0;
         }
 
-        public ulong GetLength(string sym)
+        public UIntPtr GetLength(string sym)
         {
             /*foreach (SymbolProvider sp in symbol_providers)
             {
@@ -138,10 +116,10 @@ namespace tysos
             if (sym_to_length.ContainsKey(sym))
                 return sym_to_length[sym];
             else
-                return 0;
+                return (UIntPtr)0;
         }
 
-        public string GetSymbol(ulong address)
+        public string GetSymbol(void* address)
         {
             foreach (SymbolProvider sp in symbol_providers)
             {
@@ -150,10 +128,10 @@ namespace tysos
                     return ret;
             }
 
-            return (string)offset_to_sym[address];
+            return (string)offset_to_sym[(UIntPtr)address];
         }
 
-        public string GetSymbolAndOffset(ulong address, out ulong offset)
+        public string GetSymbolAndOffset(void* address, out UIntPtr offset)
         {
             foreach (SymbolProvider sp in symbol_providers)
             {
@@ -162,24 +140,24 @@ namespace tysos
                     return ret;
             }
 
-            if (offset_to_sym.ContainsKey(address))
+            if (offset_to_sym.ContainsKey((UIntPtr)address))
             {
-                offset = 0;
-                return offset_to_sym[address];
+                offset = (UIntPtr)0;
+                return offset_to_sym[(UIntPtr)address];
             }
 
-            offset_to_sym.Add(address, "probe");
-            int idx = offset_to_sym.IndexOfKey(address);
+            offset_to_sym.Add((UIntPtr)address, "probe");
+            int idx = offset_to_sym.IndexOfKey((UIntPtr)address);
             offset_to_sym.RemoveAt(idx);
 
             if (idx == 0)
             {
-                offset = address;
+                offset = (UIntPtr)address;
                 return "offset_0";
             }
 
-            ulong sym_addr = offset_to_sym.Keys[idx - 1];
-            offset = address - sym_addr;
+            var sym_addr = offset_to_sym.Keys[idx - 1];
+            offset = libsupcs.OtherOperations.Sub((UIntPtr)address, sym_addr);
             return offset_to_sym[sym_addr];
         }
     }
