de24038d04a641062595cc079589be1a760c3c0c
jncronin <jncronin@tysos.org>
2020-11-15 13:19:39 +0000

    Move readline to main folder

--- tysila4/readline/build.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [string]$p1 = "Debug"
-)
-
-dotnet restore
-dotnet build ".\src\ReadLine" -c $p1
-dotnet build ".\src\ReadLine.Demo" -c $p1
-dotnet build ".\test\ReadLine.Tests" -c $p1
--- tysila4/readline/src/ReadLine/IAutoCompleteHandler.cs
+++
@@ -1,8 +0,0 @@
-namespace System
-{
-    public interface IAutoCompleteHandler
-    {
-        char[] Separators { get; set; }
-        string[] GetSuggestions(string text, int index);
-    }
-}
