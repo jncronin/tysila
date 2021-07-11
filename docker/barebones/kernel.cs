1ed1a57579d72669a061ffec9c3a63e2aacddab9
jncronin <jncronin@tysos.org>
2021-07-11 20:42:13 +0100

    Change barebones to use a statically allocated struct Console to demonstrate use of non-heap objects

--- docker/barebones/kernel.cs
+++ docker/barebones/kernel.cs
@@ -1,32 +1,51 @@
 namespace BareBones
 {
-    class Program
-    {
-        static int pos = 0;
- 
-        unsafe static void Main()
-        {
-            // Clear the screen
-            for(int i = 0; i < 80 * 25 * 2; i++)
-                *(byte *)(0xb8000 + i) = 0;
- 
-            // Say hi
-            Print("Hello World!");
+	unsafe struct Console
+	{
+		byte *fb;
 
-            while(true);
-        }
- 
-        static void Print(string s)
-        {
-            foreach(char c in s)
-                Print(c);
-        }
- 
-        unsafe static void Print(char c)
-        {
-            *(byte *)(0xb8000 + pos) = (byte)c;
-            *(byte *)(0xb8000 + pos + 1) = 0x0f;
-            pos += 2;
-        }
-    }
+		int pos;
+
+		public Console(byte *framebuffer)
+		{
+			fb = framebuffer;
+			pos = 0;
+		}
+
+		public void Clear()
+		{
+			for(int i = 0; i < 80 * 25 * 2; i++)
+				*(fb + i) = 0;
+		}
+
+		public void Print(string s)
+		{
+			foreach(char c in s)
+				Print(c);
+		}
+
+		public void Print(char c)
+		{
+			*(byte *)(fb + pos) = (byte)c;
+			*(byte *)(fb + pos + 1) = 0x0f;
+			pos += 2;
+		}
+	}
+		
+	unsafe class Program
+	{
+		static int pos = 0;
+
+		static Console c = new Console((byte *)0xb8000);
+
+		unsafe static void Main()
+		{
+			c.Clear();
+
+			c.Print("Hello World!");
+
+			while(true);
+		}
+	}
 }
+
